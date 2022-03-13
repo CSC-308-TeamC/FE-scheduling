486ccdc0c11ffd6f6ab877f2bbaa84daf2bc5398
Mary Lemmer <mlemmer@calpoly.edu>
2022-03-13 14:54:20 -0700

    Removed old table format.

--- src/Components/DogTable.js
+++ src/Components/DogTable.js
@@ -48,47 +48,4 @@ function TableBody(props) {
       );
 }
 
-/*function Table(props) { 
-    return (
-      <table>
-        <TableHeader />
-        <TableBody dogData={props.dogData} removeDog={props.removeDog}/>
-      </table>
-    );
- }
-
-function TableHeader() {
-    return (
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Breed</th>
-          <th>Dog</th>
-          <th></th>
-        </tr>
-      </thead>
-    );
-}
-
-function TableBody(props) {
-  const rows = props.dogData.map((row, index) => {
-    return (
-      <tr key={index}>
-        <td>{row.name}</td>
-        <td>{row.breed}</td>
-        <td>{row.dogName}</td>
-        <td>
-          <button onClick={() => props.removeDog(index)}>Delete</button>
-        </td>
-      </tr>
-    );
-  });
-  
-  return (
-      <tbody>
-        {rows}
-      </tbody>
-    );
-}*/
-
 export default formTable;
