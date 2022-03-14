9499bd29e8a576fd1b2b16f16b469f90b10a4978
PhilippeWS <91435899+PhilippeWS@users.noreply.github.com>
2022-03-14 14:43:45 -0700

    Added the dashboard panel css and changes for demonstration. These will be fixed/removed later as fit.

--- src/Components/DashboardPanel.js
+++ src/Components/DashboardPanel.js
@@ -1,182 +1,77 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import ChartistGraph from "react-chartist";
+// import { Container, Row, Col } from 'react-bootstrap';
 import Clock from 'react-live-clock';
-import 'bootstrap/dist/css/bootstrap.min.css';
-//import Card from 'react-bootstrap/Card';
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import CardActions from "@material-ui/core/CardActions";
-// import { Avatar, Box, Grid, Typography } from '@mui/material';
-// import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
-// import PeopleIcon from '@mui/icons-material/PeopleOutlined';
+import panelPic from '../imgs/panelPic.jpg';
+import {
+  Card,
+  Container,
+  Row,
+  Col,
+} from "react-bootstrap";
+import '../chartist.css';
+import '../index.css';
+<link rel="stylesheet" href="//cdn.jsdelivr.net/chartist.js/latest/chartist.min.css"></link>
 
-// export default function DashboardPanel(props) { 
-//   <>
-//   <Card border="primary" style={{ width: '18rem' }}>
-//     <Card.Header>Header</Card.Header>
-//     <Card.Body>
-//       <Card.Title>Primary Card Title</Card.Title>
-//       <Card.Text>
-//         Some quick example text to build on the card title and make up the bulk
-//         of the card's content.
-//       </Card.Text>
-//     </Card.Body>
-//   </Card>
-//   <br />
-//   </>
-//   return (
-//     // <div class="flex-container">
-//     <Container>
-//       <Row>
-//         <Col>
-//           <div id="date">
-//             <Clock format={'dddd, MMMM Do YYYY, h:mm a'} ticking={true} timezone={'US/Pacific'} />
-//           </div>
-//           <div>
-            
-//           </div>
-//         </Col>
-//       </Row>
-      
-//     </Container>
-    
-//   );
-  
 
-// }
-
-  
-export default function App() {
+function DashboardPanel(props) { 
   return (
-  
-           //<div class="flex-container">
-          // <Container>
-          //   <Row>
-          //     <Col>
-          //       <div id="date">
-          //         <Clock format={'dddd, MMMM Do YYYY, h:mm a'} ticking={true} timezone={'US/Pacific'} />
-          //       </div>
-          //       <div>
-                  
-          //       </div>
-          //     </Col>
-          //   </Row>
-            
-          // </Container>
-
-    <div style={{}}>
-      <h4>Dog Grooming Admin Dashboard</h4>
-      
-      <Card
-        style={{
-          width: 5000,
-          backgroundColor: "yellowgreen",
-        }}
-      >
-        <CardContent>
-          <Typography
-            style={{ fontSize: 14 }}
-            color="textSecondary"
-            gutterBottom
-          >
-            Greetings of the day
-          </Typography>
-          <Typography variant="h5" component="h2">
-            Dog Grooming Dashboard
-          </Typography>
-          <CardActions>
-        {/* //  <Button size="big">Stay Safe.....</Button> */}
-          <Card size = "big"> </Card>
+    // <div class="flex-container">
+    <Container>
+      <Row>
+        <Col xs={8}>
           <div id="date">
-                  <Clock format={'dddd, MMMM Do YYYY, h:mm a'} ticking={true} timezone={'US/Pacific'} />
+            <Clock format={'dddd, MMMM Do YYYY, h:mm a'} ticking={true} timezone={'US/Pacific'} />
+          </div>
+        </Col>
+        <Col xs={4}>
+          <div id="pieChart">
+            <Card.Title as="h4" id="CardTitle">Today's Appointments</Card.Title>
+            <Card.Body>
+                <div>
+                  <ChartistGraph
+                    data={{
+                      labels: ["3", "1", "2"],
+                      series: [3, 1, 2],
+                    }}
+                    type="Pie"
+                  />
                 </div>
-        </CardActions>
-          {/* <div id="date">
-                  <Clock format={'dddd, MMMM Do YYYY, h:mm a'} ticking={true} timezone={'US/Pacific'} />
-                </div> */}
-          <Typography
-            style={{
-              marginBottom: 12,
-            }}
-            color="textSecondary"
-          >
-            Keep Motivated
-          </Typography>
-          <Typography variant="body2" component="p">
-            Stay Happy
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">Stay Safe.....</Button>
-        </CardActions>
-      </Card>
-    </div>
+                <div className="legend" id="CardTitle">
+                  <span class="dot" id="yellowDot"></span>
+                  Groom
+                  <span class="dot" id="pinkDot"></span>
+                  Bath
+                  <span class="dot" id="redDot"></span>
+                  Nails
+                </div>
+              </Card.Body>
+            </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={8}>
+          <div id="date">
+          <img src={panelPic} id="dashboardPic" alt="img" width="95%" height="90%"/>
+          </div>
+        </Col>
+        <Col xs={4}>
+          <div id="nextAppt">
+            <Card.Title as="h2" id="CardTitle">Next Appointment</Card.Title>
+            <div id="nextApptInfo">
+              Time: 1:15 pm
+              <br />
+              Dog Breed: Wetterhoun
+              <br />
+              Appointment Type: Nails
+              <br />
+              Checked in: Yes
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
-
-
-// export const TotalCustomers = (props) => (
-//   <Card {...props}>
-//     <CardContent>
-//       <Grid
-//         container
-//         spacing={3}
-//         sx={{ justifyContent: 'space-between' }}
-//       >
-//         <Grid item>
-//           <Typography
-//             color="textSecondary"
-//             gutterBottom
-//             variant="overline"
-//           >
-//             TOTAL CUSTOMERS
-//           </Typography>
-//           <Typography
-//             color="textPrimary"
-//             variant="h4"
-//           >
-//             1,6k
-//           </Typography>
-//         </Grid>
-//         <Grid item>
-//           <Avatar
-//             sx={{
-//               backgroundColor: 'success.main',
-//               height: 56,
-//               width: 56
-//             }}
-//           >
-//             <PeopleIcon />
-//           </Avatar>
-//         </Grid>
-//       </Grid>
-//       <Box
-//         sx={{
-//           alignItems: 'center',
-//           display: 'flex',
-//           pt: 2
-//         }}
-//       >
-//         <ArrowUpwardIcon color="success" />
-//         <Typography
-//           variant="body2"
-//           sx={{
-//             mr: 1
-//           }}
-//         >
-//           16%
-//         </Typography>
-//         <Typography
-//           color="textSecondary"
-//           variant="caption"
-//         >
-//           Since last month
-//         </Typography>
-//       </Box>
-//     </CardContent>
-//   </Card>
-// );
-//export default DashboardPanel;
+export default DashboardPanel;
