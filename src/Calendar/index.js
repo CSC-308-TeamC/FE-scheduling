29bd256f0fde3c309ca5f0b76c369302f6781a8d
Arun-Ganesh07 <74291980+Arun-Ganesh07@users.noreply.github.com>
2022-03-13 17:41:55 -0700

    Revert "Task calendar update"

--- src/Calendar/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import store from './store/index.js';
-import App from "./App";
-import {Provider} from 'react-redux';
-
-
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
