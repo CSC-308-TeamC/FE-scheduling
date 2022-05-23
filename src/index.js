e387aa8f8433fc2757b45fafb91bddce73c8db89
CBrazell23 <colebrazell@gmail.com>
2022-05-23 11:56:51 -0700

    styling

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 ReactDOM.render(
   <CookiesProvider>
     {" "}
-    <div style={{ height: "100vh", backgroundColor: "rgb(236,230,255)" }}>
-      <Dashboard />{" "}
-    </div>
+    {/* <div style={{ height: "100vh", backgroundColor: "rgb(236,230,255)" }}> */}
+    <Dashboard />{" "}
+    {/* </div> */}
   </CookiesProvider>,
   document.getElementById("root")
 );
